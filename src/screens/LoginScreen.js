e2b0d08b5564c84b512a89c7f476e5b343bbcabd
Andy Lin <andy.lin.2004@outlook.com>
2025-10-23 19:11:37 -0400

    better login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,17 +1,36 @@
+import ScreenContainer from "@/components/ScreenContainer";
 import { useState } from "react";
-import { StyleSheet, TextInput, View } from "react-native";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function LoginScreen() {
     const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-        <View>
-            <TextInput
-                value={username}
-                onChangeText={setUsername}
-                placeholder="Message"
-            />
-        </View>
-    )
+        <ScreenContainer>
+            <View style={styles.centered}>
+                <TextInput
+                    value={username}
+                    onChangeText={setUsername}
+                    placeholder="Username"
+                    placeholderTextColor="#6b7280"
+                    style={styles.input}
+                />
+                <TextInput
+                    value={password}
+                    onChangeText={setPassword}
+                    placeholder="Password"
+                    style={styles.input}
+                    placeholderTextColor="#6b7280"
+                    secureTextEntry={true}
+                />
+                <View/>
+                <TouchableOpacity style={styles.btn}>
+                    <Text style={styles.btnText}>Login</Text>
+                </TouchableOpacity>
+            </View>
+        </ScreenContainer>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -19,10 +38,22 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#111827',
         borderRadius: 999,
-        paddingHorizontal: 14,
-        paddingVertical: 10,
+        padding: 14,
         color: '#e5e7eb',
         borderWidth: 1,
-        borderColor: '#1f2937'
+        borderColor: '#1f2937',
+        marginBottom: 10,
     },
+    btn: {
+        backgroundColor: '#38bdf8',
+        borderRadius: 12,
+        paddingVertical: 12,
+        alignItems: 'center',
+        marginBottom: 12
+    },
+    btnText: { color: '#0b1220', fontWeight: '700' },
+    centered: {
+        marginTop: 'auto',
+        marginBottom: 'auto',
+    }
 });
